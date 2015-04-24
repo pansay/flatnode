73ceb36be92ec442aa18f56dcc168a10c5aef817
pansay <jan.krepelka@gmail.com>
2015-04-24 23:15:40 +0200

    model

--- controller/router.js
+++ controller/router.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     router = express.Router(),
+    files = require('./../model/files'),
     texts = require('./../language/en'),
     config = require('./../config'),
     sprintf = require('sprintf-js').sprintf,
     data = {
         txt: texts
     };
+
+var posts = files.listFiles(config.postsFolder);
+console.log(posts);
 
 module.exports = router
 
--- model/files.js
+++ model/files.js
@@ -1,1 +1,18 @@
-//
+var fs = require('fs'),
+    path = require('path');
+
+module.exports = {
+
+    // list files
+    listFiles: function listFiles (dir) {
+        var arr = [];
+        fs.readdirSync(dir).forEach(function(file) {
+            var dirpath = path.join(dir, file);
+            if (fs.statSync(dirpath).isFile()) {
+                arr.push('/' + dirpath);
+            }
+        });
+        return arr;
+    }
+
+}
